b903a75a4ee1419739fa1b629b0265b3a3987f90
aditya123473892 <adityathakur6199@gmail.com>
2024-06-20 19:56:33 +0530

    master

--- utils/validateMongodbId.js
+++ utils/validateMongodbId.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const validateMongooseId = (id) =>{
     const isValid = mongoose.Types.ObjectId.isValid(id);
     if(!isValid){
-        throw new Error("Id not found")
+        throw new Error("Id nott found")
     }
 }
 
